f936201d8cfeb34862fd9852faa105f648878cc6
Prakash harvani <prakashharvani1997@gmail.com>
2024-11-29 14:11:14 +0530

    used server actions in saperate file

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,21 +1,10 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import shareMeal from '@/lib/actions';
 
 export default function ShareMealPage() {
 
-  function shareMeal(formData) {
-    'use server'
-    const meal = {
-      title:formData.get('title'),
-      summary:formData.get('summary'),
-      creator:formData.get('name'),
-      creator_email:formData.get('email'),
-      instructions:formData.get('instructions'),
-      image:formData.get('image'),
-
-    }
-    console.log('------meal',meal)
-  }
+ 
 
   return (
     <>
